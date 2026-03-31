543416d3022916f200ef5541f5e94a79cd671839
Riehl  Kevin <kriehl@ethz.ch>
2026-03-31 10:28:22 +0200

    update logs

--- src/figures/Figure_1.py
+++ src/figures/Figure_1.py
@@ -124,6 +124,31 @@ df_neg_kar = load_data(
     factor=factors[1]
 )
 controller_dfs_10_rw1 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
+df_respect = load_data(
+    grid_size="15", 
+    controller="DECENTRALIZED_RESPECT", 
+    measure=row_measures[0],  
+    factor=factors[1]
+)
+df_neg_ego = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_EGOISTIC",
+    measure=row_measures[0],  
+    factor=factors[1]
+)
+df_neg_alt = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_ALTRUISTIC",
+    measure=row_measures[0],  
+    factor=factors[1]
+)
+df_neg_kar = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_TRIP_KARMA",
+    measure=row_measures[0],  
+    factor=factors[1]
+)
+controller_dfs_15_rw1 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
 
 df_respect = load_data(
     grid_size="5", 
@@ -175,6 +200,31 @@ df_neg_kar = load_data(
     factor=factors[1]
 )
 controller_dfs_10_rw2 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
+df_respect = load_data(
+    grid_size="15", 
+    controller="DECENTRALIZED_RESPECT", 
+    measure=row_measures[1],   
+    factor=factors[0]
+)
+df_neg_ego = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_EGOISTIC",
+    measure=row_measures[1],   
+    factor=factors[0]
+)
+df_neg_alt = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_ALTRUISTIC",
+    measure=row_measures[1],   
+    factor=factors[0]
+)
+df_neg_kar = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_TRIP_KARMA",
+    measure=row_measures[1],   
+    factor=factors[0]
+)
+controller_dfs_15_rw2 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
 
 df_respect = load_data(
     grid_size="5", 
@@ -218,6 +268,27 @@ df_neg_kar = load_data(
     measure=row_measures[2],      
 )
 controller_dfs_10_rw3 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
+df_respect = load_data(
+    grid_size="15", 
+    controller="DECENTRALIZED_RESPECT", 
+    measure=row_measures[2],     
+)
+df_neg_ego = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_EGOISTIC",
+    measure=row_measures[2],     
+)
+df_neg_alt = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_ALTRUISTIC",
+    measure=row_measures[2],     
+)
+df_neg_kar = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_TRIP_KARMA",
+    measure=row_measures[2],     
+)
+controller_dfs_15_rw3 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
 
 df_respect = load_data(
     grid_size="5", 
@@ -261,7 +332,27 @@ df_neg_kar = load_data(
     measure=row_measures[3], 
 )
 controller_dfs_10_rw4 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
-
+df_respect = load_data(
+    grid_size="15", 
+    controller="DECENTRALIZED_RESPECT", 
+    measure=row_measures[3],     
+)
+df_neg_ego = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_EGOISTIC",
+    measure=row_measures[3],      
+)
+df_neg_alt = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_ALTRUISTIC",
+    measure=row_measures[3],      
+)
+df_neg_kar = load_data(
+    grid_size="15",
+    controller="DECENTRALIZED_NEGOTIATE_TRIP_KARMA",
+    measure=row_measures[3],     
+)
+controller_dfs_15_rw4 = [df_respect, df_neg_ego, df_neg_alt, df_neg_kar]
 
 plt.figure(figsize=(FIGURE_WIDTH, FIGURE_HEIGHT))
 
@@ -420,21 +511,73 @@ for idx, controller_df in enumerate(controller_dfs_10_rw4):
 
 plt.subplot(3, 4, 1 + 4 * 2)
 plt.ylabel("15x15 Grid", fontweight="bold")
-plt.xlim(1, 80 - 1)
 plt.xlabel("Density [#Agents]")
-
+for idx, controller_df in enumerate(controller_dfs_15_rw1):
+    plt.plot(
+        controller_df["n"],
+        controller_df["mean"],
+        label=controller_labels[idx],
+        color=controller_colors[idx],
+    )
+    plt.fill_between(
+        controller_df["n"],
+        controller_df["mean"] - controller_df["std"],
+        controller_df["mean"] + controller_df["std"],
+        color=controller_colors[idx],
+        alpha=0.1,
+    )
+    
 plt.subplot(3, 4, 2 + 4 * 2)
-plt.xlim(1, 80 - 1)
 plt.xlabel("Density [#Agents]")
-
+for idx, controller_df in enumerate(controller_dfs_15_rw2):
+    plt.plot(
+        controller_df["n"],
+        controller_df["mean"],
+        label=controller_labels[idx],
+        color=controller_colors[idx],
+    )
+    plt.fill_between(
+        controller_df["n"],
+        controller_df["mean"] - controller_df["std"],
+        controller_df["mean"] + controller_df["std"],
+        color=controller_colors[idx],
+        alpha=0.1,
+    )
+    
 plt.subplot(3, 4, 3 + 4 * 2)
-plt.xlim(1, 80 - 1)
 plt.xlabel("Density [#Agents]")
-
+for idx, controller_df in enumerate(controller_dfs_15_rw3):
+    plt.plot(
+        controller_df["n"],
+        controller_df["mean"],
+        label=controller_labels[idx],
+        color=controller_colors[idx],
+    )
+    plt.fill_between(
+        controller_df["n"],
+        controller_df["mean"] - controller_df["std"],
+        controller_df["mean"] + controller_df["std"],
+        color=controller_colors[idx],
+        alpha=0.1,
+    )
+    
 plt.subplot(3, 4, 4 + 4 * 2)
-plt.xlim(1, 80 - 1)
 plt.xlabel("Density [#Agents]")
-
+for idx, controller_df in enumerate(controller_dfs_15_rw4):
+    plt.plot(
+        controller_df["n"],
+        controller_df["mean"],
+        label=controller_labels[idx],
+        color=controller_colors[idx],
+    )
+    plt.fill_between(
+        controller_df["n"],
+        controller_df["mean"] - controller_df["std"],
+        controller_df["mean"] + controller_df["std"],
+        color=controller_colors[idx],
+        alpha=0.1,
+    )
+    
 plt.subplots_adjust(top=0.960, bottom=0.060, left=0.060, right=0.990, hspace=0.250, wspace=0.230)
 plt.savefig("Figure_1.png", dpi=300)
 plt.show()
